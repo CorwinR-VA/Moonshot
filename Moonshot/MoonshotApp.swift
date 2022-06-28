a1e8513878d2c52949ed9c4bab5e14d772451826
Corwin Rainier <corwinrainier89@gmail.com>
2022-06-28 10:44:23 -0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Corwin Rainier on 6/28/22.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
